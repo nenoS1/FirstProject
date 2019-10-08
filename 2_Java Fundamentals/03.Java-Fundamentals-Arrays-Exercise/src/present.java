80731f4196c55e9b8ce676761fcbffb7c93372fb
Neno <neno.shirtev@gmail.com>
2019-10-08 10:04:46 +0300

    .

--- 2_Java Fundamentals/03.Java-Fundamentals-Arrays-Exercise/src/present.java
+++ 2_Java Fundamentals/03.Java-Fundamentals-Arrays-Exercise/src/present.java
@@ -3,10 +3,16 @@ import java.util.Scanner;
 public class present {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] numbers = new int[2];
+
+        int countOfNumbers = scanner.nextInt();
+        int[] numbers = new int[countOfNumbers];
 
         for (int i = 0; i < numbers.length; i++) {
             numbers[i]=scanner.nextInt();
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] *= 2;
         }
 
         for (int i = numbers.length-1; i >= 0; i--) {
