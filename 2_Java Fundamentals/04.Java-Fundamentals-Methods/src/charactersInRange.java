5769ecdff092f8cfef08d1f90361b7c92a379778
Neno <neno.shirtev@gmail.com>
2019-10-13 20:34:25 +0300

    .

---
+++ 2_Java Fundamentals/04.Java-Fundamentals-Methods/src/charactersInRange.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class charactersInRange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
--- 2_Java Fundamentals/04.Java-Fundamentals-Methods/src/vowelsCount.java
+++ 2_Java Fundamentals/04.Java-Fundamentals-Methods/src/vowelsCount.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class vowelsCount {
+
+    static void printVowelsCount(String input){
+        String[] vowels = {"a", "e", "i", "o", "u"};
+        String s = input;
+        String[] word = s.split("");
+        int count = 0;
+
+        for (String word1  : vowels) {
+            for (String word2 : word) {
+                if(word1.equals(word2)){
+                    count += 1;
+                }
+            }
+        }
+        System.out.print(count);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine().toLowerCase();
+
+        printVowelsCount(text);
+
+    }
 }
