6c922e72fc8a6e1514a46db5f15680a8c21c95b2
Neno <neno.shirtev@gmail.com>
2019-10-29 15:11:38 +0200

    .

--- 2_Java Fundamentals/06.Java-Fundamentals-Objects-and-Classes/src/Article.java
+++ 2_Java Fundamentals/06.Java-Fundamentals-Objects-and-Classes/src/Article.java
@@ -1,8 +1,27 @@
 import java.util.Scanner;
 
 public class Article {
+
+    String Title;
+    String Content;
+    String Author;
+
+    public void edit(String x){
+        System.out.println("Hello");
+    }
+
+    static void changeAuthor(String y){
+
+    }
+
+    static void rename(String z){
+
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
+        Article article = new Article();
+        article.
     }
 }
--- 2_Java Fundamentals/06.Java-Fundamentals-Objects-and-Classes/src/advertisementMessage.java
+++ 2_Java Fundamentals/06.Java-Fundamentals-Objects-and-Classes/src/advertisementMessage.java
@@ -9,37 +9,40 @@ public class advertisementMessage {
         String Events;
         String Authors;
         String Cities;
-    }
 
-    static void genMsg(int x){
-        String[] Phrases = {"Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can’t live without this product."};
-        String[] Events = {"Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!"};
-        String[] Authors = {"Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"};
-        String[] Cities = {"Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"};
+        static void genMsg(int x){
+            String[] Phrases = {"Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can’t live without this product."};
+            String[] Events = {"Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!"};
+            String[] Authors = {"Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"};
+            String[] Cities = {"Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"};
 
-        List<AdMessage> msg = new ArrayList<>();
+            List<AdMessage> msg = new ArrayList<>();
 
-        Random rnd = new Random();
+            Random rnd = new Random();
 
-        AdMessage message = new AdMessage();
-        message.Phrases = Phrases[rnd.nextInt(Phrases.length)];
-        message.Events = Events[rnd.nextInt(Events.length)];
-        message.Authors = Authors[rnd.nextInt(Authors.length)];
-        message.Cities = Cities[rnd.nextInt(Cities.length)];
+            AdMessage message = new AdMessage();
+            message.Phrases = Phrases[rnd.nextInt(Phrases.length)];
+            message.Events = Events[rnd.nextInt(Events.length)];
+            message.Authors = Authors[rnd.nextInt(Authors.length)];
+            message.Cities = Cities[rnd.nextInt(Cities.length)];
 
-        msg.add(message);
+            msg.add(message);
 
-        for (AdMessage adMessage : msg) {
-            System.out.printf("%s %s %s - %s%n", message.Phrases, message.Events, message.Authors, message.Cities);
+            for (AdMessage adMessage : msg) {
+                System.out.printf("%s %s %s - %s%n", message.Phrases, message.Events, message.Authors, message.Cities);
+            }
         }
     }
+
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numMsgs = Integer.parseInt(scanner.nextLine());
 
+
         for (int i = 0; i < numMsgs; i++) {
-            genMsg(numMsgs);
+            advertisementMessage.AdMessage.genMsg(numMsgs);
         }
     }
 }
