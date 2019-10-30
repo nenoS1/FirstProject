b8d11a30e8f2c09d792a837aa7ea0280fabb0035
Neno <neno.shirtev@gmail.com>
2019-10-30 11:19:28 +0200

    new class

--- 2_Java Fundamentals/06.Java-Fundamentals-Objects-and-Classes/src/Article.java
+++ 2_Java Fundamentals/06.Java-Fundamentals-Objects-and-Classes/src/Article.java
@@ -1,27 +1,79 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Article {
 
-    String Title;
-    String Content;
-    String Author;
 
-    public void edit(String x){
-        System.out.println("Hello");
+    static class Articles{
+        String Title;
+        String Content;
+        String Author;
+
+        public Articles(String Title, String Content, String Author) {
+            this.Title = Title;
+            this.Content = Content;
+            this.Author = Author;
+        }
+
+        public String getTitle() {
+            return Title;
+        }
+
+        public void setTitle(String title) {
+            Title = title;
+        }
+
+        public String getContent() {
+            return Content;
+        }
+
+        public void setContent(String content) {
+            Content = content;
+        }
+
+        public String getAuthor() {
+            return Author;
+        }
+
+        public void setAuthor(String author) {
+            Author = author;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s - %s: %s", this.Title, this.Content, this.Author);
+        }
     }
 
-    static void changeAuthor(String y){
-
-    }
-
-    static void rename(String z){
-
-    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split(", ");
+        int numOperations = Integer.parseInt(scanner.nextLine());
 
-        Article article = new Article();
-        article.
+        Articles article = new Articles(input[0],input[1], input[2]);
+        List<Articles> art = new ArrayList<>();
+        art.add(article);
+
+        while(numOperations-- > 0){
+            String[] tokens = scanner.nextLine().split(": ");
+            String command = tokens[0];
+            String param = tokens[1];
+
+            switch (command){
+                case "Edit" : article.setContent(param);
+                    break;
+                case "ChangeAuthor": article.setAuthor(param);
+                    break;
+                case "Rename": article.setTitle(param);
+                    break;
+            }
+        }
+
+        System.out.println(article);
+
     }
 }
---
+++ 2_Java Fundamentals/06.Java-Fundamentals-Objects-and-Classes/src/OpinionPoll.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class OpinionPoll {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
