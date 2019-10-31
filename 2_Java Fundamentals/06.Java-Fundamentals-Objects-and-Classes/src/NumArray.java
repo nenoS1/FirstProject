6f76cb7a68f31e09173881f59c91ee92fe730c7b
Neno <neno.shirtev@gmail.com>
2019-10-31 10:09:16 +0200

    .

--- 2_Java Fundamentals/06.Java-Fundamentals-Objects-and-Classes/src/NumArray.java
+++ 2_Java Fundamentals/06.Java-Fundamentals-Objects-and-Classes/src/NumArray.java
@@ -1,6 +1,34 @@
 import java.util.Scanner;
 
 public class NumArray {
+
+    static void printSumNeg(int[] IN){
+        int sum = 0;
+        for (int i = 0; i < IN.length; i++) {
+            if (IN[i]<0){
+                sum += IN[i];
+            }
+        }
+        System.out.println(sum);
+    }
+
+    static void printSumPos(int[] IN){
+        int sum = 0;
+        for (int i = 0; i < IN.length; i++) {
+            if (IN[i]>=0){
+                sum += IN[i];
+            }
+        }
+        System.out.println(sum);
+    }
+
+    static void printSumAll(int[] IN){
+        int sum = 0;
+        for (int i = 0; i < IN.length; i++) {
+            sum += IN[i];
+        }
+        System.out.println(sum);
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -14,26 +42,36 @@ public class NumArray {
         while (!"End".equals(line=scanner.nextLine())){
             String[] tokens = line.split(" ");
             String operations = tokens[0];
-            int index = Integer.parseInt(tokens[1]);
+            String index1 = tokens[1];
 
             switch (operations){
                 case "Switch" :
-                    if(index <= input.length-1){
+                    int index = Integer.parseInt(tokens[1]);
+                    if(index>=0 && index <= input.length-1){
                         input[index] = -(input[index]);
                     }
                     break;
                 case "Change" :
+                    index = Integer.parseInt(tokens[1]);
                     int value = Integer.parseInt(tokens[2]);
-                    if(index <= input.length-1){
+                    if(index>=0 && index <= input.length-1){
                         input[index] = value;
                     }
                     break;
                 case "Sum" :
+                    if (index1.equals("Negative")){
+                        printSumNeg(input);
+                    } else if (index1.equals("Positive")){
+                        printSumPos(input);
+                    } else {
+                        printSumAll(input);
+                    }
                     break;
 
             }
         }
         for (int i = 0; i < input.length; i++) {
+            if(input[i]>=0)
             System.out.print(input[i]+" ");
         }
     }
