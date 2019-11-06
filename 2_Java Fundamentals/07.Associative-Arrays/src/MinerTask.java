b7830e41d5a9641cb466dc96f1bc602e4921cc7a
Neno <neno.shirtev@gmail.com>
2019-11-06 16:56:18 +0200

    .

--- 2_Java Fundamentals/07.Associative-Arrays/src/MinerTask.java
+++ 2_Java Fundamentals/07.Associative-Arrays/src/MinerTask.java
@@ -1,9 +1,35 @@
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class MinerTask {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
 
+        Map<String, Integer> counts = new LinkedHashMap<>();
+        List<String> evenRes = new ArrayList<>();
+        List<Integer> oddQty = new ArrayList<>();
+
+        int linenumber = 0;
+        String line;
+        while (!"stop".equals(line = scanner.nextLine())) {
+            //String[] input = Arrays.stream(line.split(" ")).toArray(String[]::new);
+
+            if (linenumber % 2 == 0) {
+                evenRes.add(line);
+                } else {
+                oddQty.add(Integer.parseInt(line));
+            }
+                linenumber = linenumber + 1;
+        }
+
+        for (int i = 0; i < evenRes.size(); i++) {
+            counts.put(evenRes.get(i),oddQty.get(i));
+        }
+            for (Map.Entry<String, Integer> stringIntegerEntry : counts.entrySet()) {
+                System.out.printf("%s -> %d%n", stringIntegerEntry.getKey(), stringIntegerEntry.getValue());
+
+
+            }
+        }
     }
-}
