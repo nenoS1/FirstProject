8aec62cdc76098f09512adcfd3d8560292aaef95
Neno <neno.shirtev@gmail.com>
2019-11-06 12:55:49 +0200

    .

--- 2_Java Fundamentals/07.Associative-Arrays/src/CountCharsInString.java
+++ 2_Java Fundamentals/07.Associative-Arrays/src/CountCharsInString.java
@@ -1,10 +1,24 @@
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class CountCharsInString {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        HashMap<>
+        char[] charcount = scanner.nextLine().toCharArray();
+
+        TreeMap<Character, Integer> counts = new TreeMap<>();
+
+        for (char c : charcount) {
+            if (c != ' ') {
+                if (!counts.containsKey(c)) {
+                    counts.put(c, 0);
+                }
+                counts.put(c, counts.get(c) + 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> characterIntegerEntry : counts.entrySet()) {
+            System.out.printf("%c -> %s%n", characterIntegerEntry.getKey(), characterIntegerEntry.getValue());
+        }
     }
 }
