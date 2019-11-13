20a1371523ec99802ec8afd75e7342377b9b8f4e
Neno <neno.shirtev@gmail.com>
2019-11-13 11:21:52 +0200

    .

--- 2_Java Fundamentals/07.Associative-Arrays/src/LegendaryFarming.java
+++ 2_Java Fundamentals/07.Associative-Arrays/src/LegendaryFarming.java
@@ -1,8 +1,13 @@
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class LegendaryFarming {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        Map<String, List<Integer>> res = new LinkedHashMap<>();
 
     }
 }
